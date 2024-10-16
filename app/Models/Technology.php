11e4aae4a53c02c69633a4138bbc140d65cf5080
andrearomeo04 <pisco004@gmail.com>
2024-10-16 18:14:01 +0200

    [UPD] created migration and technology model function

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'slug'];
+
+    public static function generateSlug($title){
+        return Str::slug($title, '-');
+    }
+
+    public function projects(){
+        return $this->belongsToMany(Project::class);
+    }
 }
